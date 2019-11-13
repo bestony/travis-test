0c1a4677875e27f3af7ee9735f474bf283436629
bestony <best.tony@foxmail.com>
2019-11-13 10:40:27 +0800

    feat: add code

--- .github/release.sh
+++ .github/release.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 echo "Ready Package Examples"
+mkdir packages
+for dir in $(ls -d */ | sed 's#/##'); do
+
+    cd $dir;
+    echo "go To $dir"
+    for dir2 in $(ls -d */ | sed 's#/##'); do
+        echo "package $dir/$dir2"
+        zip -r "../packages/$dir2.zip" $dir2
+    done
+    echo "go To Parent Directory"
+    cd ..
+
+done
